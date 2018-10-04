d8924534df4b83f751e6a2202b211337290d0ae6
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 02:31:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 1; i <= names.length; i++) {
+    console.log(`Welcome ${names[i-1]}! You are employee #${i}.`)
+  }
+  return names
+}
+
+function tailsNeverFails() {
+  function flipCoin() {
+    return Math.random() >=.5 ? "Tails" : "Heads"
+  }
+
+  let counter = 0
+
+  while(flipCoin() === "Tails") {
+    counter++
+  }
+  return `You got ${counter} tails in a row!`
+}
