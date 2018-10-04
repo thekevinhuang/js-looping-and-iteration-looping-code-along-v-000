9bd256cdb463a3abde846616de47c2156bff9206
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 02:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solutions in this file
 function printBadges(names) {
-  for (let i = 1, i <= names.length, i++) {
-    console.log(`Welcome ${names[i-1]}! You are employee #${i}.`)
-  }
-  return names
+
 }
 
 function tailsNeverFails() {
