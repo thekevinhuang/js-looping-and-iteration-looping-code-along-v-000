c9896dc6d374ee9ea19be36f33e8f99d20375e5b
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 02:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solutions in this file
-function printBadges() {
-
+function printBadges(names) {
+  for (let i = 1, i <= names.length, i++) {
+    
+  }
 }
 
 function tailsNeverFails() {
-  
+
 }
