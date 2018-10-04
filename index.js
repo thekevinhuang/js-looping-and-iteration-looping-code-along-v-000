f292a7a6e7567b7902517c2087e09d443dc6b737
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 02:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(names) {
   for (let i = 1, i <= names.length, i++) {
-    
+    console.log(`Welcome ${names[i-1]}! You are employee #${i}.`)
   }
 }
 
