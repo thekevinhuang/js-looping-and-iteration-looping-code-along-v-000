ee62334dd916a5f089aab437de14f502cb72e5c6
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 02:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ function printBadges(names) {
 }
 
 function tailsNeverFails() {
+  function flipCoin() {
+    return Math.random >=.5 ? "Tails" : "Heads"
+  }
 
+  let counter = 0
+
+  while(flipCoin === "Tails") {
+    counter++
+  }
+  return `You got ${counter} tails in a row!`
 }
